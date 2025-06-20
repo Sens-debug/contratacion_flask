6f12dced922ca79d6fd71451d71b4aec4b22d76d
Sens-debug <jsantamariamunera@gmail.com>
2025-06-20 07:46:44 -0500

    zip front

--- backend/server.py
+++ backend/server.py
@@ -560,4 +560,4 @@ def obtener_todos_usuario():
         return jsonify({"mensaje":f"Error en el metodo{e}"}),400
 
 if __name__ == '__main__':  
-    app.run(debug=True,  host= '0.0.0.0' ,port=5009)
+    app.run(debug=True,  host= '0.0.0.0' ,port=9000)
