e6c2c478b91866186d9b66f6c6e0ddba3c0b6337
Sens-debug <jsantamariamunera@gmail.com>
2025-06-20 15:57:04 -0500

    add server proxy

--- frontend/src/modulos/Inicio_sesion.jsx
+++ frontend/src/modulos/Inicio_sesion.jsx
@@ -12,15 +12,9 @@ export function Formulario_login(){
     //Con este hook manejamos la navegacion por el sistema
     const navegacion = useNavigate();
     const uri_flask = import.meta.env.VITE_URL_SERVIDOR
-    const [valido, setValido] = useState()
-
-    useEffect(()=> {
-        fetch(uri_flask+"/comprobacion").then(response => response.json()).then(data => setValido(data.estado) )
-    },[])
-
 
     //Creamos una funcion que nos haga el manejo de envio de datos desde ellogin hasta el backend
-    const manejarEnvio =(variableControl) => {
+   const manejarEnvio =(variableControl) => {
         variableControl.preventDefault();
         //Hace un fetch a la url del backend
         fetch(uri_flask+'/inicio_sesion',{
@@ -47,34 +41,21 @@ export function Formulario_login(){
             setMensaje('Ocurrio un error durante el fetch')
         });
     };
-        if (valido== false){
-            return <h1 className=" text-red-600 bg-white text-center">ERROR</h1>
-        }
+    
+       
 
         return(
-            <div className=" bg-gray-700 justify-center min-h-screen flex items-center">
-                
-
+            <div className=" bg-gray-700 justify-center min-h-screen flex items-center">               
                 <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md flex items-center">
-                
-                {/* Creamos la etiqueta Formulario donde crearemos toda la estructura del mismo */}
                 <form onSubmit={manejarEnvio}className="space-y-4 ">
-                {/* Div del usuario */}
                 <div>
-                    {/* Creamos la etiqueta Usuario del formulario */}
                     <label className="block text-black font-medium" >Usuario:</label>
-                    {/* Creamos un input y asiganos sus valores clave
-                    -tipo-
-                    -valor{muestra el valor actual contenido en la variable citada}
-                    -onchange-{'actualiza el estado cuando el usuario escribe (on change)
-                    -requerido : Especifica campo obligatorio} */}
                     <input type="text"
                     className="mt-1 block rounded-md bg-gray-200 border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500"
                     value={usuario}
                     onChange={(e)=> setUsuario(e.target.value)} 
                     required/>
                 </div>
-                {/* DIV de la contraseña */}
                 <div>
                     <label className="block text-black font-medium">Contraseña:</label>
                     <input type="password"
@@ -87,7 +68,6 @@ export function Formulario_login(){
                 <button className="w-full bg-blue-700 text-white py-2 px-4 rounded-md hover:bg-blue-900 transition" type="submit">Ingresar</button>
                 <p className="text-m">{mensaje}</p>
                 </form>
-                
                 </div>
             </div>
             
